697374c0c2979d8d7b5e296f1f8d1ec41f204d28
ARAssotllo <55189099+ARAssotllo@users.noreply.github.com>
2019-09-11 14:20:39 +0200

    Version 1

--- Exercise2/exercise2.c
+++ Exercise2/exercise2.c
@@ -1,5 +1,12 @@
 /* Here, you should entry the required libraries */
-
+#include<stdio.h>
 void main(){
 	/* Here, you should write the source code of your program */
+	int a,b,A;
+	printf("Enter value of first number");
+	scanf("%d",&a);
+	printf("Enter value of second number");
+	scanf("%d",&b);
+	A=a+b;
+	printf("The result of the sum is %d",A);
 }
